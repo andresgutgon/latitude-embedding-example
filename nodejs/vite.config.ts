ecebfd143fe51ff5eaf6bc6cd9465c8465cd0bef
andresgutgon <andresgutgon@gmail.com>
2024-03-25 17:26:52 +0100

    try to fix build

--- nodejs/vite.config.ts
+++ nodejs/vite.config.ts
@@ -1,41 +1,24 @@
 import path from 'path'
-import honox from 'honox/vite'
-import pages from '@hono/vite-cloudflare-pages'
+import { islandComponents } from 'honox/vite'
 import { defineConfig } from 'vite'
 
 const root = './'
 export default defineConfig(({ mode }) => {
-  if (mode === 'client') {
-    return {
-      build: {
-        rollupOptions: {
-          input: {
-            styles: '/app/style.css',
-            client: '/app/client.ts',
-          },
-          manifest: true,
-          output: {
-            assetFileNames: 'static/assets/[name].[ext]',
-          },
+  return {
+    build: {
+      rollupOptions: {
+        input: {
+          server: '/app/server.ts',
+          styles: '/app/style.css',
+          client: '/app/client.ts',
+        },
+        manifest: true,
+        output: {
+          entryFileNames: '[name].js',
+          assetFileNames: 'static/assets/[name].[ext]',
         },
       },
-    }
-  } else {
-    return {
-      plugins: [
-        honox({
-          islandComponents: {
-            isIsland: (id) => {
-              const resolvedPath = path.resolve(root).replace(/\\/g, '\\\\')
-              const regexp = new RegExp(
-                `${resolvedPath}[\\\\/]app[^\\\\/]*[\\\\/]islands[\\\\/].+\.tsx?$`,
-              )
-              return regexp.test(path.resolve(id))
-            },
-          },
-        }),
-        pages()
-      ],
-    }
+    },
+    plugins: [islandComponents()],
   }
 })
